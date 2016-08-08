d71de5a06d207cac7b3bfd0ff1e28f60be2e4e90
Robbie Wolfe <repwolfe@gmail.com>
2016-08-08 15:06:49 -0400

    Android studio project creation
    	new file:   .gitignore
    	new file:   .idea/.name
    	new file:   .idea/compiler.xml
    	new file:   .idea/copyright/profiles_settings.xml
    	new file:   .idea/encodings.xml
    	new file:   .idea/gradle.xml
    	new file:   .idea/misc.xml
    	new file:   .idea/modules.xml
    	new file:   .idea/runConfigurations.xml
    	new file:   app/.gitignore
    	new file:   app/build.gradle
    	new file:   app/proguard-rules.pro
    	new file:   app/src/androidTest/java/com/studioussoftware/paperescaper/ApplicationTest.java
    	new file:   app/src/main/AndroidManifest.xml
    	new file:   app/src/main/java/com/studioussoftware/paperescaper/GameActivity.java
    	new file:   app/src/main/res/mipmap-hdpi/ic_launcher.png
    	new file:   app/src/main/res/mipmap-mdpi/ic_launcher.png
    	new file:   app/src/main/res/mipmap-xhdpi/ic_launcher.png
    	new file:   app/src/main/res/mipmap-xxhdpi/ic_launcher.png
    	new file:   app/src/main/res/mipmap-xxxhdpi/ic_launcher.png
    	new file:   app/src/main/res/values/colors.xml
    	new file:   app/src/main/res/values/strings.xml
    	new file:   app/src/main/res/values/styles.xml
    	new file:   app/src/test/java/com/studioussoftware/paperescaper/ExampleUnitTest.java
    	new file:   build.gradle
    	new file:   gradle.properties
    	new file:   gradle/wrapper/gradle-wrapper.jar
    	new file:   gradle/wrapper/gradle-wrapper.properties
    	new file:   gradlew
    	new file:   gradlew.bat
    	new file:   settings.gradle

---
+++ app/src/main/java/com/studioussoftware/paperescaper/GameActivity.java
@@ -0,0 +1,12 @@
+package com.studioussoftware.paperescaper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
